d40ecfd2de200fe730054deb9fbc78345fe89310
Noveled <noveled@naver.com>
2024-09-19 16:47:58 +0900

    디자인 소폭 수정

--- src/components/Filter/Detail.jsx
+++ src/components/Filter/Detail.jsx
@@ -43,7 +43,7 @@ const Detail = () => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(viewData),
-  };
+  }; 
 
   useEffect(() => {
     dispatch(fetchUpdateViewcount(options));
